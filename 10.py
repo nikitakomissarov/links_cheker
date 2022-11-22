817999dba70afaea5cf91ed7663d74dfbfd1cb82
nikitakomissarov <59535117+nikitakomissarov@users.noreply.github.com>
2022-11-22 16:45:22 +0200

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,28 +1,57 @@
+import self as self
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-import re
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
+import requests
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
-print("dfsdpdfsdfs \n dfsds")
 link = "http://ouds.alm.su/"
-browser = webdriver.Chrome()
-browser.get(link)
-html = browser.page_source
-time.sleep(1)
-print(html)
 
-header_burger = browser.find_element(By.CLASS_NAME, 'header__burger').click()
 
-time.sleep(5)
-browser.maximize_window()
 
-expression = " Объекты "
 
-objects = browser.find_element(By.CSS_SELECTOR, "a[href='/objects/']").click()
-ActionChains(browser).move_to_element(objects).key_down(Keys.COMMAND).click(objects).key_up(Keys.COMMAND).perform()
-company = browser.find_element(By.CSS_SELECTOR, "a[href='/company/']").click()
-services = browser.find_element(By.CSS_SELECTOR, "a[href='/services/']").click()
-news = browser.find_element(By.CSS_SELECTOR, "a[href='/news/']").click()
-browser.quit()
+
+
+
+
+class Pages():
+
+    def __init__(self):
+        self.link = link
+
+    def just_start(self, link):
+        self.link = link
+        print("it's going")
+        browser = webdriver.Chrome()
+        browser.get(link)
+        browser.implicitly_wait(5)
+        return browser
+
+
+    def link_testingfirst(self, browser):
+        browser.maximize_window()
+        try:
+            burger_bar_links = browser.find_elements(By.CLASS_NAME, "header__nav_link")
+
+
+            for inlink in burger_bar_links:
+                if (requests.head(inlink.get_attribute('href')).status_code == 200):
+                    print("Link is Valid")
+                    print(inlink.get_attribute('href'))
+                    browser.implicitly_wait(5)
+
+                else:
+                    print("Link is Invalid/Broken")
+
+        finally:
+            browser.quit()
+
+
+
+
+if __name__ == "__main__":
+    start = Pages()
+    start.link_testingfirst(start.just_start(link))
+
+
