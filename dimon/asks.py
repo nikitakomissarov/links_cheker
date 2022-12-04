562009897fb34859c98dcb123f38437726ac751d
nikitakomissarov <59535117+nikitakomissarov@users.noreply.github.com>
2022-12-04 19:32:41 +0300

    1.0 version
    finished parsing tool that able to check the whole site for invalid links in it

--- dimon/asks.py
+++ dimon/asks.py
@@ -1,42 +1,64 @@
-
 import requests
+from requests.exceptions import MissingSchema
+from requests.exceptions import InvalidSchema
+from requests.exceptions import ConnectionError
 from selenium.webdriver.common.by import By
 
-from selenium import webdriver
-
-# class Links:
-#
-#     def __init__(self, url):
-#         self.url = url
-#
-#     def requesting(self):
-#         html = urllib.request.urlopen(self.url).read()
-#         soup = BeautifulSoup(html, 'html.parser')
-#         linksbufer = soup.findAll('a')
-#         for link in linksbufer:
-#             print(link.get('href'))
 
 class Links:
 
     def __init__(self, browser):
         self.browser = browser
 
-    def requesting(self):
-        burger_bar_links = self.browser.find_elements(By.XPATH, "//a")
-        linklist = list(burger_bar_links)
-        for link in burger_bar_links:
-            try:
-                a = requests.head(link.get_attribute('href'))
-                if a.status_code == 200:
-                    clearlink = link.get_attribute('href')
-                    print("Link is Valid:", clearlink)
-                    if clearlink not in linklist:
-                        linklist.append(clearlink)
-                    else:
-                        continue
-                else:
-                    print("Link is Invalid/Broken:", clearlink, link.get_attribute('outerHTML'))
-            except:
-                print('SomethingElseGetsWrong:', link.get_attribute('href'), link.get_attribute('outerHTML'))
+    def requesting(self, key, clean_links=None, invalid_links=None, foreign_links=None, wrong_links=None):
+        try:
+            if clean_links is None:
+                clean_links = list()
+            if invalid_links is None:
+                invalid_links = list()
+            if foreign_links is None:
+                foreign_links = list()
+            if wrong_links is None:
+                wrong_links = list()
 
-        return linklist
+            burger_bar_links = self.browser.find_elements(By.XPATH, "//a")
+
+            for link in burger_bar_links:
+                check = link.get_attribute('href')
+
+                if check not in clean_links and check not in foreign_links:
+                    try:
+                        a = requests.head(link.get_attribute('href')).status_code == 200
+                        cleanlink = link.get_attribute('href')
+                        print(f'LINK VALID {cleanlink}')
+                        # print("Link is Valid:", cleanlink)
+                        if cleanlink.startswith(key) and cleanlink not in clean_links:
+                            clean_links.append(cleanlink)
+                        else:
+                            foreign_links.append(cleanlink)
+                            continue
+
+                    except InvalidSchema:
+                        wrong_link = link.get_attribute('href'), link.get_attribute('outerHTML')
+                        if wrong_link not in wrong_links:
+                            wrong_links.append(wrong_link)
+                            print(f'InvalidSchema {wrong_link}')
+
+                    except MissingSchema:
+                        wrong_link = link.get_attribute('href'), link.get_attribute('outerHTML')
+                        if wrong_link not in wrong_links:
+                            wrong_links.append(wrong_link)
+                            print(f'InvalidSchema {wrong_link}')
+
+                    except ConnectionError:
+                        print(f'ConnectionError:" {link.get_attribute("href"), link.get_attribute("outerHTML")}')
+                        invalidlink = link.get_attribute('href'), link.get_attribute('outerHTML'),
+                        if invalidlink not in invalid_links:
+                            invalid_links.append(invalidlink)
+                else:
+                    continue
+        finally:
+            self.browser.quit()
+        return clean_links, invalid_links, foreign_links, wrong_links
+
+
